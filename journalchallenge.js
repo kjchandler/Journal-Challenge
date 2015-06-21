fe37c56e38b780c574dcf6ffdc4ed81026ea6a5f
Kelly Chandler <chandlerkelly@hotmail.com>
2015-06-20 22:48:39 -0400

    almost done

--- journalchallenge.js
+++ journalchallenge.js
@@ -1,41 +1,88 @@
 //create a journal object
 
   function Journal() 
-  {		this.entries = [];
+  {		
+      this.entries = [];
 
   		//adds an Entry with the given info
-  		this.addEntry = function adds(title, content, author, id) {
+  		this.addEntry = function adds(title, content, author, tags) {
 
   			//create an entry object
-  			var entry = new Entry(title, content, author, id)
+  			var entry = new Entry(title, content, author, tags);
   			//add an entry to an array
-  			this.entry.push(entry);
+  			this.entries.push(entry);
 
   		}
 
   		//Displays the entry.
   		this.displayEntry = function showEntry(entry) {
   			console.log("\t"+entry.title + "\n");
-  		}
+        console.log("\t"+entry.author);
+        console.log(entry.content);
+  		
+      //display the id of the entry
+      for (var i = 0; i < entry.tags.length; i++){
+          console.log(entry.tags[i]);
+      }
+      console.log("**********************");
 
   }
 
+  //Display an array of entries
+  this.displayEntries = function showEntries(entryArray){
+    for (var i = 0; i < entryArray.length; i++){
+        //display the entry
+        this.displayEntry(entryArray[i]);
+    }
+  }
+
+  //display entries in the journal
+  this.displayAllEntries = function showAllEntries(){
+    this.displayEntries(this.entries);
+  }
+
+  //find all entries with a given tag
+  this.findAllEntriesWithTags = function findTag(tag){
+
+    var foundEntries = [];
+
+    //look at entry
+    for(var i = 0; i < this.entries.length; i++) {
+      var currentEntry = this.entries[i];
+
+      // add it to the array if id found
+      if(currentEntry.hasTag(tag)){
+        foundEntries.push(currentEntry);
+      }
+    }
+    // return all entries with an id
+    return foundEntries;
+  }
+}
 
   //Create an entry constructor function
-  function Entry(title, content, author, id) {
+  function Entry(title, content, author, tags) {
   	this.title = title;
   	this.content = content;
   	this.author = author;
-  	this.id = id;
+  	this.tags = tags;
 
-  	//the id is to identify a specific entry
-  	this.entryid = function (id){
-  		for (var i = 0; i < this.id.length; i++){
-  			if (this.id[i])===tag) {
-				return true;
+  	//identify a specific entry
+  	this.hasTag = function (tag){
+  		for (var i = 0; i < this.tags.length; i++){
+  			 if (this.tags[i] === tag) {
+				    return true;
 
-			}
+			   }
   		}
   		return false;
   	}
 }
+
+
+//create a new journal
+var myJournal = new Journal();
+myJournal.addEntry("June 20,2015", "How to create a constructor function", "Kelly",["Saturday", "Weekend"]);
+console.log(myJournal);
+
+
